5648ea94bb3b4758fc3a18379c1b9ccaf2b56596
Yan-Ying Liao <n9102125@gmail.com>
2022-08-31 16:55:26 +0800

    remove comments

--- main.py
+++ main.py
@@ -1,19 +1,3 @@
-# import subprocess
-
-# host = "www.goo12345gle.com"
-host = "go-germany-api.onrender.com"
-
-# ping = subprocess.Popen(
-#     ["ping", "-c", "4", host],
-#     stdout = subprocess.PIPE,
-#     stderr = subprocess.PIPE
-# )
-
-# out, error = ping.communicate()
-# print (out)
-
-# import platform    # For getting the operating system name
-# import subprocess  # For executing a shell command
 import logging
 import requests
 logging.basicConfig(level=logging.INFO,
@@ -24,25 +8,7 @@ logging.basicConfig(level=logging.INFO,
                     ]
                     )
 
-# def ping(host):
-#     """
-#     Returns True if host (str) responds to a ping request.
-#     Remember that a host may not respond to a ping (ICMP) request even if the host name is valid.
-#     """
 
-#     # Option for the number of packets as a function of
-#     param = '-n' if platform.system().lower()=='windows' else '-c'
-
-#     # Building the command. Ex: "ping -c 1 google.com"
-#     command = ['ping', param, '1', host]
-
-#     return subprocess.call(command) == 0
-
-if __name__ == "__main__":
-    # res = ping(host)
+if __name__ == "__main__":    
     r = requests.get('https://go-germany-api.onrender.com/status')
     logging.info(r)
-    # if res:
-    #     logging.info(f"Ping {host} successfully.")
-    # else:
-    #     logging.warning(f"{host} does not respond.")

ce731f87d87945bc5cddd71df345e24221f744d1
Yan-Ying Liao <n9102125@gmail.com>
2022-08-31 20:58:54 +0800

    use deta to run cron job

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
 import logging
 import requests
-logging.basicConfig(level=logging.INFO,
-                    format="%(asctime)s [%(levelname)s] %(message)s",
-                    handlers=[
-                        logging.FileHandler("status.log"),
-                        logging.StreamHandler()
-                    ]
-                    )
+from deta import app
+# logging.basicConfig(level=logging.INFO,
+#                     format="%(asctime)s [%(levelname)s] %(message)s",
+#                     handlers=[
+#                         logging.FileHandler("status.log"),
+#                         logging.StreamHandler()
+#                     ]
+#                     )
 
 
-if __name__ == "__main__":    
-    r = requests.get('https://go-germany-api.onrender.com/status')
-    logging.info(r)
+url = 'https://go-germany-api.onrender.com/status'
+
+def get(url):
+    r = requests.get(url)
+    print(r)
+    # logging.info(r)
+
+@app.lib.cron()
+def cron_job(event):
+    get(url)
+    # return "running on a schedule"
+
+# if __name__ == "__main__":    
+#     get(url)
+    
